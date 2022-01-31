fce10bd9d734c09b9c7ddf1b0de4d6e32cd6610d
Ajay Melekamburath <ajaymk16@iisertvm.ac.in>
2022-01-31 23:14:04 +0530

    Initial Commit

---
+++ run_jupyter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Submitting the bash file
+jobid=$(sbatch --parsable submit.sh)
+echo "Submitting the job file"
+logfile=Jupyter-$jobid.out
+
+# Waitng for Jupyter to load and connet
+while true ; do
+    echo "Working..."
+    sleep 5s
+    if grep -q "token=" "$logfile"; then
+        echo " "
+        echo "Copy and paste the following command to your local terminal:"
+        head -n 1 $logfile
+        echo " "
+        echo "Copy and paste either or links below to your browser:"
+        tail -n 2 $logfile
+        echo " "
+        echo "To kill the job, use: scancel $jobid"
+        echo " "
+        break
+    fi
+    echo "Working..."
+    sleep 2s
+done
+
+
+# By Ajay (https://github.com/ajay-mk)
